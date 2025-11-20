9f03ccd1c1bbe3ba28c7be17c11f590976cea6fb
Calebcp <calebroemhildt@yahoo.com>
2025-11-20 23:04:50 +0800

    popular name

---
+++ guesspopular.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+
+
+int main()
+{
+	char name[50];
+	printf("Enter the name and see if it is Popular in the USA\n");
+	scanf("%s", &name);
+	
+	char popular[][20] = {"John", "James", "Mathew", "Mary", "Taylor"};
+	int ispopular = 0;
+	
+	for (int i =0; i<5;i++)
+	{
+		if (strcmp(name, popular[i])==0) {
+			ispopular = 1;
+			break;
+		}
+	}
+	printf("%s is a popular name in the USA \n", name);
+	return 0;
+	
+}
---
+++ testcompiler.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdio.h>
+
+int main() {
+    printf("We are going to draw some shapes with just some Asterisks\n");
+    int i, j, rows;
+    //Drawing a square
+    rows = 5;
+    printf("Square:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < rows; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    //drawing a right triangle
+    printf("Right Triangle:\n");
+    for (i = 1; i <= rows; i++) {
+        for (j = 1; j <= i; j++) {
+            printf("* ");
+
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    //drawing an equilateral triangle
+    printf("Equilateral Triangle:\n");
+    for (i = 1; i <= rows; i++) {
+        for (j = i; j < rows; j++) {
+            printf("");
+        }
+        for (j = 1; j <= (2 * i - 1); j++) {
+            printf("*");
+
+        }
+        printf("\n");
+
+    }
+    //draw a sword
+    printf("\nSword:\n");
+    printf("   *   \n");
+    printf("   *   \n");
+    printf("   *   \n");
+    printf("   *   \n");
+    printf("*******\n");
+    printf("   *   \n");
+    printf("   *   \n");
+
+
+return 0;
+}
+    
