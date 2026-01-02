bd2316bf5d1ea323bdf7d8beb445a030eae6d0bd
Calebcp <calebroemhildt@yahoo.com>
2026-01-02 23:42:01 +0800

    more pointers

--- pointerarithmetic.c
+++ pointerarithmetic.c
@@ -25,7 +25,19 @@ int main() {
         }  
         printf("\n");  
     }  
-  
+  	int meal;
+	float tax;
+	float tip;
+	float total;
+	printf("Enter the Total Meal Cost: ");
+	scanf("%d", &meal);
+	
+	tax = meal * .05;
+	tip = meal * .18;
+	total = tax + tip + meal;
+	printf("Tax: %.1f\n Tip: %.1f\n Total Cost of Meal: %.1f\n", tax, tip, total);
+	
+	return 0;
     return 0;  
 }  
 
