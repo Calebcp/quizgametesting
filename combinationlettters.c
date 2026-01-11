61450efa62b12f4d8704ef7fbfb9120c609e6ece
Calebcp <calebroemhildt@yahoo.com>
2026-01-11 23:28:50 +0800

    add combinations

--- combinationlettters.c
+++ combinationlettters.c
@@ -17,5 +17,38 @@ int main() {
                 break;  
             }  
         }  
-       return 0;
+        if (valid) {  
+            for (int i = 0; s2[i] != '\0'; i++) {  
+                if (!((s2[i] >= '0' && s2[i] <= '9') || (s2[i] >= 'A' && s2[i] <= 'Z') || (s2[i] >= 'a' && s2[i] <= 'z'))) {  
+                    valid = 0;  
+                    break;  
+                }  
+            }  
+        }  
+    }  
+      
+    if (!valid) {  
+        printf("Wrong Input!\n");  
+    } else {  
+        for (int i = 0; s1[i] != '\0'; i++) {  
+            exist[(unsigned char)s1[i]] = 1;  
+        }  
+        for (int i = 0; s2[i] != '\0'; i++) {  
+            exist[(unsigned char)s2[i]] = 1;  
+        }  
+          
+        for (char c = '0'; c <= '9'; c++) {  
+            if (exist[(unsigned char)c]) {  
+                result[idx++] = c;  
+            }  
+        }  
+        for (char c = 'A'; c <= 'Z'; c++) {  
+            if (exist[(unsigned char)c]) {  
+                result[idx++] = c;  
+            }  
+        }  
+         
+    }  
+      
+    return 0;  
 }  
