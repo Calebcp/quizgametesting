74d9181011683bdfdfb88e3381c69ebea8dfd6f6
Calebcp <calebroemhildt@yahoo.com>
2026-01-23 23:42:52 +0800

    extar feature

--- guessbuscolor.c
+++ guessbuscolor.c
@@ -8,7 +8,7 @@ int main() {
     srand(time(0));
     int random_idx = rand() % total_colors;
     char *target_color = colors[random_idx];
-    
+
     char *colors[] = {"red", "blue", "green", "yellow", "black", "white"};
     int total_colors = 6;
     char guess[20];
@@ -24,6 +24,31 @@ int main() {
         printf("Congratulations! You guessed the right color!\n");
     } else {
         printf("Wrong guess! The bus color is %s.\n", target_color);
+    }
+
+     while (attempts > 0 && !correct) {
+        printf("Attempt %d/%d - Enter your guess: ", 4 - attempts, 3);
+        scanf("%19s", guess);
+        to_lower(guess);
+
+        if (!is_valid_guess(guess, colors, total_colors)) {
+            printf("Invalid input! Choose only from the listed colors.\n");
+            continue;
+        }
+
+        if (strcmp(guess, target_color) == 0) {
+            correct = 1;
+            printf("\n🎉 Congratulations! You guessed correctly!\n");
+        } else {
+            attempts--;
+            if (attempts > 0) {
+                printf("❌ Wrong! You have %d attempt(s) left.\n", attempts);
+            }
+        }
+    }
+
+    if (!correct) {
+        printf("\n💥 Game Over! The bus color was %s.\n", target_color);
     }
 
     return 0;
